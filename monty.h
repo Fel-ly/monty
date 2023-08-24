f02458adc31e3476e02c2341cf291c014a80bb1c
Fel-ly <kiokofpm@gmail.com>
2023-08-24 14:29:47 -0700

    header file

--- monty.h
+++ monty.h
@@ -59,5 +59,6 @@ void f_pop(stack_t **head, unsigned int counter);
 void f_swap(stack_t **head, unsigned int counter);
 void f_add(stack_t **head, unsigned int counter);
 void f_nop(stack_t **head, unsigned int counter);
+int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
 
 #endif
